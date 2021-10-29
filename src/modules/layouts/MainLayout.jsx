486275bb4136897c15b237791cc9127c0eb00356
Adrien Guéret <adrien.grt@gmail.com>
2021-10-29 21:34:21 +0200

    feat(about): add 'About' page

--- src/modules/layouts/MainLayout.jsx
+++ src/modules/layouts/MainLayout.jsx
@@ -10,6 +10,7 @@ const Logo = styled.img`
 `;
 
 const Content = styled.section`
+${({ theme }) => theme.typography.variants.body1}
    padding: ${({ theme }) => theme.spacing(0, 5, 5, 5) };
 `;
 
@@ -20,10 +21,43 @@ const Header = styled.header`
     align-items: center;;
 `;
 
-const HeaderLink = styled.a`
+const HeaderLink = styled(Link)`
+    ${({ theme }) => theme.typography.variants.body1}
+    color: inherit;
+    font-weight: bold;
+    text-decoration: none;
+    display: inline-block;
+    padding: ${({ theme }) => theme.spacing(1, 0) };
+    position: relative;
+
+    margin-left: auto;
+    margin-right: ${({ theme }) => theme.spacing(4) };
+    white-space: nowrap;
+    
+    &::after {
+        content: '';
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        height: 2px;
+        background-color: currentColor;
+        transition: transform 300ms;
+        transform-origin: right center;
+        transform: scale(0);
+    }
+
+    &:hover,&:focus {
+        &::after {
+            transform-origin: left center;
+            transform: scale(1);
+        }
+    }
+`;
+
+const TwitterLink = styled.a`
     color: inherit;
     transition: color 300ms;
-    margin-left: auto;
     
     & svg {
         transition: transform 300ms;
@@ -32,7 +66,7 @@ const HeaderLink = styled.a`
 
     &:hover,&:focus {
         color: #00acee;
-        
+
         & svg {
             transform: scale(1.3);
         }
@@ -46,11 +80,14 @@ function MainLayout({ children }) {
                 <Link to="/" title="Retour à l'accueil" aria-label="Retour à l'accueil">
                     <Logo src={logo} alt="Mario Universalis" />
                 </Link>
-                <HeaderLink href="https://twitter.com/MarioUnivRsalis" title="@MarioUnivRsalis" aria-label="Compte Twitter : @MarioUnivRsalis">
+                <HeaderLink to="/a-propos">
+                    À propos
+                </HeaderLink>
+                <TwitterLink href="https://twitter.com/MarioUnivRsalis" title="@MarioUnivRsalis" aria-label="Compte Twitter : @MarioUnivRsalis">
                     <svg width="32" height="32" fill="currentColor" viewBox="0 0 25 25">
                         <path d="M22.46 6c-.77.35-1.6.58-2.46.69.88-.53 1.56-1.37 1.88-2.38-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29 0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15 0 1.49.75 2.81 1.91 3.56-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07 4.28 4.28 0 0 0 4 2.98 8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21 16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56.84-.6 1.56-1.36 2.14-2.23z" />
                     </svg>
-                </HeaderLink>
+                </TwitterLink>
             </Header>
 
             <Content>{ children }</Content>
---
+++ src/pages/a-propos.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Metas from '../modules/app/components/Metas';
+import MainLayout from '../modules/layouts/MainLayout';
+import { Block } from '../modules/ui';
+
+const CenteredBlock = styled(Block)`
+  margin: auto;
+`;
+
+const Content = styled.div`
+  text-align: left;
+`;
+
+const Subtitle = styled.h2`
+   ${({ theme }) => theme.typography.variants.subtitle}
+   text-align: center;
+   margin: ${({ theme }) => theme.spacing(-3, 0, 3, 0)};
+   font-style: italic;
+`;
+
+export default function About() {
+ 
+
+  return (
+    <MainLayout>
+      <Metas
+        title="À propos"
+        description="Ce que vous devez savoir sur ce mini-site"
+      />
+      <CenteredBlock
+        title="À propos"
+        titleComponent="h1"
+      >
+        <Content>
+          <Subtitle>Règles de confidentialité et conditions générales d'utilisation</Subtitle>
+          <p>
+            Ce mini-site amateur a été créé pour Mario Universalis. Il n'a rien d'officiel avec Nintendo. <br />
+            Les noms des jeux, des personnages et des marques cités sur ce site sont des propriétés de Nintendo.
+          </p>
+          <p>Ce mini-site récupère les données qu'il affiche via l'<a href="https://www.mariouniversalis.fr/graphql/">API GraphQL</a> de Mario Universalis.</p>
+          <p>
+            Ce mini-site n'utilise aucun cookies. Il utilise malgré tout le <em>localStorage</em> de votre navigateur pour se rappeler de la dernière page d'où vous
+            venez (uniquement dans le cadre de ce mini-site, il n'a pas accès à votre historique de navigation complet) afin que la navigation soit 100% fonctionnelle.
+          </p>
+          <p>
+            Ce mini-site utilise l'API de YouTube pour afficher les vidéos des jeux.<br />
+            De fait, veuillez consulter <a href="https://www.youtube.com/t/terms">les conditions générales d'utilisation de YouTube</a>,
+            ainsi que <a href="https://policies.google.com/privacy">les règles de confidentialité de Google</a>.
+          </p>
+        </Content>
+      </CenteredBlock>
+    </MainLayout>
+  );
+}
