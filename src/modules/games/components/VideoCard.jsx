f49a158a3045c52dadae79746bb5680081107448
Adrien Guéret <adrien.grt@gmail.com>
2021-10-30 19:28:20 +0200

    feat(game,details): add style to play button on video thumbnails

--- src/modules/games/components/VideoCard.jsx
+++ src/modules/games/components/VideoCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import useBreakpoint from '../../ui/hooks/useBreakpoint';
+
 const Root = styled.div`
     display: flex;
     flex-direction: column;
@@ -42,9 +44,32 @@ const ChannelName = styled.p`
 
 const YouTubeIframe = styled.iframe`
     width: 100%;
+    background: black;
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+`;
 
-    ${({ theme }) => theme.breakpoints.only('xs')} {
-        height: auto;
+const PlayButton = styled.button`
+    width: 100%;
+    position: relative;
+    cursor: pointer;
+    background: black;
+    border: 0;
+    padding: 0;
+    margin: 0;
+    line-height: 0;
+
+    &:hover,&:focus {
+        img {
+            opacity: .7;
+        }
+    }
+
+    &:active {
+        img {
+            opacity: .5;
+        }
     }
 `;
 
@@ -53,6 +78,19 @@ const Thumbnail = styled.img`
 
     ${({ theme }) => theme.breakpoints.only('xs')} {
         height: auto;
+    }
+`;
+
+const SvgContainer = styled.div`
+    position: absolute;
+    width: 20%;
+    left: calc(50% - 10%);
+    top: calc(50% - 10%);
+    color: rgba(0, 0, 0, 0.7);
+    transition: color 200ms;
+
+    ${PlayButton}:hover &, ${PlayButton}:focus & {
+        color: red;
     }
 `;
 
@@ -68,6 +106,8 @@ export default function VideoCard({
     ...otherProps
 }) {
     const embedUrl = `https://www.youtube.com/embed/${videoId}?modestbranding=1&rel=0&showinfo=0&autoplay=1`;
+    const shouldRenderVideoWithSmallHeight = useBreakpoint('xs');
+    
     return (
         <Root {...otherProps}>
             { isPlaying ? (
@@ -79,15 +119,32 @@ export default function VideoCard({
                     frameBorder="0"
                     autoplay="1"
                     allow="autoplay; fullscreen"
+                    style={{ backgroundImage: `url(${thumbnailUrl})` }}
                 />    
             ) : (
-                <Thumbnail
-                    width={thumbnailWidth}
-                    height={thumbnailHeight}
-                    src={thumbnailUrl}
-                    title={title}
-                    onClick={onPlay}
-                />
+                <PlayButton onClick={onPlay}>
+                    <Thumbnail
+                        width={thumbnailWidth}
+                        height={thumbnailHeight}
+                        src={thumbnailUrl}
+                        title={title}
+                    />
+                    <SvgContainer>
+                        <svg
+                            version="1.1"
+                            viewBox="0 0 68 48"
+                        >
+                            <path
+                                d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z"
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M 45,24 27,14 27,34"
+                                fill="#fff"
+                            />
+                        </svg>
+                    </SvgContainer>
+                </PlayButton>
             )}
   
             <Content>
