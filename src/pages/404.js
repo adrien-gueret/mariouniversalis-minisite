d73c1c2e5a49cdd3aec1a3683091221356394544
Adrien Guéret <adrien.grt@gmail.com>
2023-10-14 15:26:49 +0200

    feat(404): rework 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 
-const PageNotFound = () => {
-  return <>404, page not found :)</>;
-};
+import Metas from "../modules/app/components/Metas";
+import MainLayout from "../modules/layouts/MainLayout";
+import { Block } from "../modules/ui";
 
-export default PageNotFound;
+import marioImage from "./images/mario-finding-nothing.png";
+
+const CenteredBlock = styled(Block)`
+  margin: auto;
+`;
+
+const SuperMario = styled.img`
+  height: 200px;
+  vertical-align: middle;
+
+  margin: ${({ theme }) => theme.spacing(1)};
+
+  ${({ theme }) => theme.breakpoints.up("md")} {
+    height: 400px;
+  }
+`;
+
+export default function About() {
+  return (
+    <MainLayout>
+      <Metas title="Page non trouvée" />
+      <CenteredBlock title="Page non trouvée" titleComponent="h1">
+        <SuperMario src={marioImage} alt="" />
+        <p>La page que vous recherchez n'existe pas ou plus.</p>
+      </CenteredBlock>
+    </MainLayout>
+  );
+}
